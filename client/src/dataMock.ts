69c47c77689ca3e68c3d5c0e3ce6f521f986384a
SoderbergHampus <soderberg.dev@gmail.com>
2024-03-11 15:32:57 +0100

    Added method to mock user input data over n days, paramScores random and issueScore weighted result of paramScores

---
+++ client/src/dataMock.ts
@@ -0,0 +1,65 @@
+// Define types
+type TrackedData = {
+  name: string;
+  score: number;
+};
+
+type DataResponse = {
+  date: string;
+  issue: TrackedData;
+  params: TrackedData[];
+  journalEntry: string;
+};
+
+// Helper function for random
+const randomNumber = () => {
+  return Math.floor(Math.random() * 100 + 1);
+};
+
+// Mock n data entries
+export const generateMockData = (n: number) => {
+  // Define some constants
+  const paramNames = ['sleep', 'diet', 'stress'];
+  const issueName = 'headache';
+
+  const day = 1;
+  const listOfEntries: DataResponse[] = [];
+
+  // Loop through and mock n entries
+  for (let i = 0; i < n; i++) {
+    // Mock parameters randomly
+    const params: TrackedData[] = [];
+    for (let j = 0; j < paramNames.length; j++) {
+      const paramScore = randomNumber();
+      params.push({ name: paramNames[j], score: paramScore });
+    }
+
+    // Mock issueScore as weighted result of parameters
+    let issueScore = 100;
+    issueScore -=
+      params[0].score * 0.5 + params[1].score * 0.25 + params[2].score * 0.1;
+    issueScore = Math.round(issueScore);
+
+    const issue: TrackedData = { name: issueName, score: issueScore };
+
+    // Mock date
+    let date;
+    day + i < 10 ? (date = `0${day + i}`) : (date = '' + (day + i));
+    date = '2024-03-' + date;
+
+    // Mock journalentry
+    const journalEntry = `${date} | `.repeat(10);
+
+    // Create object for mocked data and add it to result list
+    const entry: DataResponse = {
+      date: date,
+      issue: issue,
+      params: params,
+      journalEntry: journalEntry,
+    };
+
+    listOfEntries.push(entry);
+  }
+
+  return listOfEntries;
+};
--- client/src/views/UserResults.tsx
+++ client/src/views/UserResults.tsx
@@ -1,4 +1,8 @@
+import { generateMockData } from '../dataMock';
+
 const UserResults = () => {
+  console.log(generateMockData(10));
+
   return (
     <h1 data-testid='main-heading' className='main__heading'>
       Your results
