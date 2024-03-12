72102c016701b6637ed3b523c33c6c6ea416509e
SoderbergHampus <soderberg.dev@gmail.com>
2024-03-13 00:08:34 +0100

    Added default start data for real database and made it create-drop for the moment, removed security. Endpoint to get all entries works

---
+++ api/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into entry (date, issue, issue_score, parameters, parameter_scores)
+values ('2024-03-01', 'headache', 60, 'sleep,diet,stress', '60,20,40');
+
+insert into entry (date, issue, issue_score, parameters, parameter_scores)
+values ('2024-03-02', 'headache', 20, 'sleep,diet,stress', '20,55,90');
+
+insert into entry (date, issue, issue_score, parameters, parameter_scores)
+values ('2024-03-01', 'headache', 45, 'sleep,diet,stress', '45,70,10');
