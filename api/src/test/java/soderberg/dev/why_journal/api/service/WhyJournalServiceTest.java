b167c5d2e08cfa88ef2b7b5878ba6931ab696016
SoderbergHampus <soderberg.dev@gmail.com>
2024-03-12 23:25:43 +0100

    Added tests for service, correct issue and score, correct parameters and scores etc

--- api/src/test/java/soderberg/dev/why_journal/api/service/WhyJournalServiceTest.java
+++ api/src/test/java/soderberg/dev/why_journal/api/service/WhyJournalServiceTest.java
@@ -3,6 +3,10 @@ package soderberg.dev.why_journal.api.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import soderberg.dev.why_journal.api.models.Entry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,4 +23,56 @@ class WhyJournalServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldHave3Parameters() {
+        int expected = 3;
+        Entry entry = service.getEntry(1).orElseThrow();
+        int actual = entry.getParameters().size();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldHaveCorrectIssue() {
+        String expected = "headache";
+        Entry entry = service.getEntry(1).orElseThrow();
+        String actual = entry.getIssue();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldHaveCorrectIssueScore() {
+        int expected = 60;
+        Entry entry = service.getEntry(1).orElseThrow();
+        int actual = entry.getIssueScore();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldHaveCorrectParameters() {
+        List<String> expected = new ArrayList<>();
+        expected.add("sleep");
+        expected.add("diet");
+        expected.add("stress");
+        Entry entry = service.getEntry(1).orElseThrow();
+        List<String> actual = entry.getParameters();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldHaveCorrectParameterScores() {
+        List<Integer> expected = new ArrayList<>();
+        expected.add(60);
+        expected.add(20);
+        expected.add(40);
+        Entry entry = service.getEntry(1).orElseThrow();
+        List<Integer> actual = entry.getParameterScores();
+
+        assertEquals(expected, actual);
+    }
+
 }
