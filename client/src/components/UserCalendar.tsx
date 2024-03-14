067553aa06fa1ee126271f0afaf6a3c10cb4b505
SoderbergHampus <soderberg.dev@gmail.com>
2024-03-14 20:44:40 +0100

    Better sections and calendar style

--- client/src/components/UserCalendar.tsx
+++ client/src/components/UserCalendar.tsx
@@ -16,38 +16,40 @@ const UserCalendar = ({ entries }: UserCalendarProps) => {
     <>
       {entriesCopy !== undefined && (
         <>
-          <h2 className='col-span-full col-start-2'>Calendar Entries:</h2>
-          <section className='col-span-10 col-start-2 grid max-h-96 grid-flow-col grid-cols-12 place-items-center overflow-scroll rounded bg-gray-semilight'>
-            <div className='col-span-full grid w-full grid-cols-5 place-items-center gap-1 py-4'>
-              <p className='col-start-1 text-xl font-medium'>Date</p>
-              <p className='col-start-2 text-xl font-medium'>Headache</p>
-              <p className='col-start-3 text-xl font-medium'>Sleep</p>
-              <p className='col-start-4 text-xl font-medium'>Diet</p>
-              <p className='col-start-5 text-xl font-medium'>Stress</p>
-            </div>
+          <section>
+            <h2 className='col-span-full col-start-2'>Calendar Entries:</h2>
+            <div className='col-span-10 col-start-2 grid max-h-96 grid-flow-col grid-cols-12 place-items-center overflow-scroll rounded bg-gray-semilight'>
+              <div className='col-span-full grid w-full grid-cols-5 place-items-center gap-1 py-4'>
+                <p className='col-start-1 text-xl font-medium'>Date</p>
+                <p className='col-start-2 text-xl font-medium'>Headache</p>
+                <p className='col-start-3 text-xl font-medium'>Sleep</p>
+                <p className='col-start-4 text-xl font-medium'>Diet</p>
+                <p className='col-start-5 text-xl font-medium'>Stress</p>
+              </div>
 
-            {entriesCopy.map((entry) => (
-              // <div
-              //   key={entry.date}
-              //   className='col-span-full grid w-full grid-cols-5 place-items-center gap-1 even:bg-gray-light'
-              // >
-              <button
-                key={entry.date}
-                onClick={() =>
-                  navigate(entry.date, {
-                    state: { date: entry.date, entry: entry },
-                  })
-                }
-                className='col-span-full grid w-full grid-cols-5 place-items-center gap-1 py-2 even:bg-gray-light'
-              >
-                <p>{entry.date}</p>
-                <p className='text-red'>{entry.issue.score}</p>
-                <p>{entry.parameters[0].score}</p>
-                <p>{entry.parameters[1].score}</p>
-                <p>{entry.parameters[2].score}</p>
-              </button>
-              // </div>
-            ))}
+              {entriesCopy.map((entry) => (
+                <button
+                  key={entry.date}
+                  onClick={() =>
+                    navigate(entry.date, {
+                      state: { date: entry.date, entry: entry },
+                    })
+                  }
+                  className='col-span-full grid w-full grid-cols-5 place-items-center gap-1 py-2 even:bg-gray-light'
+                >
+                  <p>{entry.date}</p>
+                  {entry.issue.score >= 70 ? (
+                    <p className='text-green'>{entry.issue.score}</p>
+                  ) : (
+                    <p className='text-red'>{entry.issue.score}</p>
+                  )}
+                  <p>{entry.parameters[0].score}</p>
+                  <p>{entry.parameters[1].score}</p>
+                  <p>{entry.parameters[2].score}</p>
+                </button>
+                // </div>
+              ))}
+            </div>
           </section>
         </>
       )}
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
       'gray-semilight': '#d4d4d8',
       'gray-medium': '#71717a',
       red: '#dc2626',
+      green: '#16a34a',
       black: '#09090b',
     },
   },
