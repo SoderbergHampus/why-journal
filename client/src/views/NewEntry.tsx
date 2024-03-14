434e7127d13ded311cad26d53537839c8fb886b1
SoderbergHampus <soderberg.dev@gmail.com>
2024-03-14 22:21:43 +0100

    Unsaved file in last commit

--- client/src/views/NewEntry.tsx
+++ client/src/views/NewEntry.tsx
@@ -5,10 +5,8 @@ const NewEntry = () => {
   return (
     <>
       <Navbar selected='home' />
-      <section className='grid grid-cols-12 content-center'>
-        <h1 data-testid='main-heading' className='col-span-full'>
-          Journal your day
-        </h1>
+      <section className='section-view'>
+        <h1 data-testid='main-heading'>Journal your day</h1>
         <JournalIssueForm />
       </section>
     </>
